386859ad89d6885c87a7f8cf3014b75705be94b5
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2024-03-25 23:15:48 +0800

    Edited Pop and partially fixed the buttons in date range

--- src/home_reportBtn.js
+++ src/home_reportBtn.js
@@ -9,17 +9,18 @@ $(document).ready(function () {
         <div class="popup ${report}-popup">
         <div class="popup-header">
             <div class="popup-text">${reportname}</div>
-            <a href="/"> <div class="btn-close"><img src="../public/images/close.svg" alt="close popup"> </div> </a>
+            <button class="btn-close" onclick="close_Popup"><img src="../public/images/close.svg" alt="close popup"> </button>
         </div>
         <div class="popup-subheader">
             <div class="dashed-line"></div>
             <div class="popup-text-one">Date range</div>
             <div class="dashed-line"></div>
         </div>
-        <div class="popup-options">
-            <div class="btn-dropdown-mock">Month</div>
-            <div class="btn-dropdown-mock">Quarter</div>
-            <div class="btn-dropdown-mock">Year</div>
+        
+        <div class="popup-options date_range">
+            <button class="btn-dropdown-mock month" onclick="selectMonth()">Month</button>
+            <button class="btn-dropdown-mock" id="quarter">Quarter</button>
+            <button class="btn-dropdown-mock" id="year">Year</button>
         </div>
         <div class="popup-subheader">
             <div class="dashed-line"></div>
@@ -27,9 +28,9 @@ $(document).ready(function () {
             <div class="dashed-line"></div>
         </div>
         <div class="popup-options">
-            <div class="btn-dropdown-mock">Repair</div>
-            <div class="btn-dropdown-mock">Replace</div>
-            <div class="btn-dropdown-mock">Return</div>
+            <button class="btn-dropdown-mock">Repair</button>
+            <button class="btn-dropdown-mock">Replace</button>
+            <button class="btn-dropdown-mock">Return</button>
         </div>
         <div class="popup-subheader">
             <div class="popup-subheader">
@@ -44,8 +45,18 @@ $(document).ready(function () {
             </div>
         </div>
         <div class="popup-options">
-            <div class="btn-dropdown-mock">(select) <img src="../public/images/dropdownArrow.svg" alt="dropdown"></div>
-            <div class="btn-dropdown-mock">(select) <img src="../public/images/dropdownArrow.svg" alt="dropdown"></div>
+                <select class="btn-dropdown-mock">
+                    <option value="0">(Select)</option>
+                    <option value="1">Speakers</option>
+                    <option value="2">Microphones</option>
+                    <option value="3">Amplifiers</option>
+                </select>
+                <select class="btn-dropdown-mock">
+                    <option value="0">(Select)</option>
+                    <option value="1">Speakers</option>
+                    <option value="2">Microphones</option>
+                    <option value="3">Amplifiers</option>
+                </select>
         </div>
         <div class="btn-generate-report">
                 <a href="${report}.html">Generate report</a>
@@ -63,4 +74,27 @@ $(document).ready(function () {
         $(".overlay-holder").fadeOut('fast', 'swing');
         $(this).closest(".popup").remove();
     });
+
+    
 });
+
+function selectMonth(){
+    alert("hello world");
+
+    $('.date_range').replaceWith(`
+        <div class="popup-options">
+            <select class="btn-dropdown-mock">
+                <option value="default">(Select)</option>
+                <option value="jan">January</option>
+                <option value="feb">February</option>
+                <option value="mar">March</option>
+            </select>
+            <select class="btn-dropdown-mock">
+                <option value="default">(Select)</option>
+                <option value="24">2024</option>
+                <option value="23">2023</option>
+                <option value="22">2022</option>
+            </select>
+        </div>
+    `);
+}
