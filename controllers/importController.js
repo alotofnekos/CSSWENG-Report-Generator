65e1f63104e15227d5a6c84caf4d1390849ce7aa
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-06-18 08:51:28 +0800

    Remove clutter

--- controllers/importController.js
+++ controllers/importController.js
@@ -8,19 +8,6 @@ const fs = require('fs');
 const repairModel = require('../models/repairSchema.js');
 const repairIdModel = require('../models/repairIdSchema.js');
 
-// //Check if a repair ID model exists and update it
-// async function idExists(repairId){
-//     await repairIdModel.findOneAndUpdate({
-//         idCounter: repairId,
-//     });
-// };
-
-// //Create neww repair model ID
-// async function notIdExists(){
-//     const newRepairId = new repairIdModel({});
-//     await newRepairId.save();
-// };
-
 async function insertRepair(excelValues) {
     const hatdog = parseInt(excelValues[1]);
     // console.log(hatdog);
@@ -30,16 +17,6 @@ async function insertRepair(excelValues) {
     var repairId;
     var valuesToStore = new Object();
     var valuesToSave = new Array();
-
-    //Check if a repair ID model exists. If yes return true, if no create a new one
-    // const done = await repairIdModel.findOne({}).then(id => {
-    //     console.log("awman", id);
-    //     if(id == null) {
-    //         notIdExists();
-    //     }
-
-    //     return true;
-    // });
     
     //Iterate of the length of excel values. Starts from 1 because of an additional automatic entry and excelValuesLength - 1
     //because of another additional automatic entry
@@ -54,16 +31,6 @@ async function insertRepair(excelValues) {
             && excelValues[i+21] && excelValues[i+22] && excelValues[i+23]) == "NULL") {
             console.log("no input");
         } else {
-            //If creating or finding the repair ID model is done, find and store its idCounter value to repairId 
-            // if(done) {
-            //     await repairIdModel.findOne({}).then(id => {
-            //         console.log("awman 2", id);
-                    
-            //         repairId = id.idCounter + 1;
-            //         idExists(repairId)
-            //     });
-            // };
-
             // console.log("pl no = " + excelValues[i+1]);
             // console.log(parseInt(excelValues[i+1]));
             // console.log(typeof parseInt(excelValues[i+1]));
@@ -73,35 +40,6 @@ async function insertRepair(excelValues) {
             const newRepairId = new repairIdModel();
             
             await newRepairId.save();
-
-            // const newRepair = new repairModel({
-            //     repairId: newRepairId.idCounter,
-            //     repairDate: parseInt(excelValues[i]),
-            //     // repairDate: addDays('1900-01-01', excelValues[i]?['Date']),
-            //     repairPLNumber: parseInt(excelValues[i+1]) ,//parseInt(excelValues[i+1]) 
-            //     repairCustomer: excelValues[i+2],
-            //     repairItemModel: excelValues[i+3],
-            //     repairDescription: excelValues[i+4],
-            //     repairQuantity: parseInt(excelValues[i+5]),//parseInt(excelValues[i+5])
-            //     repairUOM: excelValues[i+6],
-            //     repairPullOutBy: excelValues[i+7],
-            //     repairCategory1: excelValues[i+8],
-            //     repairCategory2: parseInt(excelValues[i+9]),
-            //     repairSerialNumber: parseInt(excelValues[i+10]), //commented out because can't be null entries
-            //     repairJobOrderNumber: parseInt(excelValues[i+11]), //commented out because can't be null entries
-            //     repairDateStarted: parseInt(excelValues[i+12]),
-            //     repairDateFinished: parseInt(excelValues[i+13]),
-            //     repairTechnician1: excelValues[i+14],
-            //     repairTechnician2: excelValues[i+15],
-            //     repairItemStatus: excelValues[i+16],
-            //     repairDeliveryStatus: excelValues[i+17],
-            //     repairRemarks: excelValues[i+18],
-            //     repairCost: parseInt(excelValues[i+19]), //commented out because can't be null entries
-            //     repairReturnFormNumber: parseInt(excelValues[i+20]), //commented out because can't be null entries
-            //     repairDateReturned: parseInt(excelValues[i+21]),
-            //     repairStatus: excelValues[i+22],
-            //     repairDefect: excelValues[i+23]
-            // });
 
             const newRepair = {
                 repairId: newRepairId.idCounter,
@@ -133,13 +71,11 @@ async function insertRepair(excelValues) {
             };
 
             valuesToSave.push(newRepair);
-            // await newRepair.save();
         };
-        
     };
     
-        //Insert new repair model into DB
-        await repairModel.insertMany(valuesToSave);
+    //Insert new repair model into DB
+    await repairModel.insertMany(valuesToSave);
 }
 
 const importController = {
@@ -165,8 +101,6 @@ const importController = {
             insertRepair(excelValues);
         });
     }
-
-    
 };
 
 //Export importController to be used
